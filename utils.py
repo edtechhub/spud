b2aee80800a73821363d534f82b448486e564560
Jan Adamiak <45169660+JanAdamiak@users.noreply.github.com>
2020-06-19 12:15:08 +0100

    Initial

--- utils.py
+++ utils.py
@@ -1,40 +1,8 @@
 from django.db.models import Q
-from django.conf import settings
+from django.db import connection
 
 import re
 
-
-def formulate_with_filter_query(key):
-	parts = settings.WITH_FILTERS[key]
-	if type(parts) == str and (" AND " in parts or " OR " in parts):
-		return join_with_filter_queries(key)
-
-	if type(parts) == str:
-		return Q(tak__iregex=parts)
-
-	q_with = Q()
-	for _q in parts["regex"]:
-		q_with |= Q(tak__regex=_q)
-
-	for _q in parts["iregex"]:
-		q_with |= Q(tak__iregex=_q)
-
-	return q_with
-
-def join_with_filter_queries(key):
-	query = settings.WITH_FILTERS[key]
-	keys = query.replace(" AND ", " ").replace(" OR ", " ").replace("(", "").replace(")", "").split()
-
-	complete_query = {}
-	for _key in keys:
-		complete_query[_key] = formulate_with_filter_query(_key)
-
-	if key == "TT and PP":
-		return ((complete_query["TT"]) & (complete_query["PP"]))
-	elif key == "TE or (TT and PP)":
-		return ((complete_query["TE"]) | (complete_query["TT"]) & (complete_query["PP"]))
-	else:
-		return Q()
 
 def formulate_tak_query(query_string):
 	query_parts = Q()
@@ -58,3 +26,45 @@ def formulate_tak_query(query_string):
 			query_parts &= Q(tak__iregex=_q)
 
 	return query_parts
+
+
+def faster_count():
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT reltuples::bigint AS __count FROM pg_class WHERE relname = 'publications';")
+        est_count = int(cursor.fetchone()[0])
+
+    return est_count
+
+
+def raw_query(author, filters, year, tak):
+	query = """SELECT publications.id, publications.title, publications.additionaltitles, publications.authors, publications.year, publications.publicationtype, publications.links, publications.importedfrom, publications.containername, publications.doi, publications.tsv, publications.recordmetadata_zbuamajorversion, publications.recordmetadata_dateretrieved, publications.recordmetadata_dateconverted, publications.recordmetadata_recordtype, publications.recordmetadata_source, publications.recordmetadata_recordname, publications.recordmetadata_searchguid, publications.recordmetadata_numberinsource, publications.recordmetadata_zbuaminorversion, publications.publisherdatecopyright, publications.location, publications.author100, publications.daterange, publications.isbn, publications.citation, publications.keywords, publications.abstract, publications.identifier, publications.itemdatatype, publications.itemdatahandler, publications.created_at FROM publications WHERE """
+	counter = 0
+	if author:
+		counter += 1
+		query = query + """publications.tsa @@ websearch_to_tsquery('""" + str(author) + """')"""
+
+	if filters:
+		counter += 1
+		if counter == 1:
+			query = query + """publications.""" + str(filters[0]) + """ >'0'"""
+			for _filter in filters:
+				query = query + """ AND """ + """publications.""" + str(_filter) + """ >'0'"""
+		else:
+			for _filter in filters:
+				query = query + """ AND """ + """publications.""" + str(_filter) + """ >'0'"""
+	if year:
+		counter += 1
+		if counter == 1:
+			query = query + """publications.year='""" + str(year) + """'"""
+		else:
+			query = query + """ AND """ + """publications.year ='""" + str(year) + """'"""
+	if tak:
+		counter += 1
+		if counter == 1:
+			query = query + """publications.tsv @@ websearch_to_tsquery('""" + str(tak) + """')"""
+		else:
+			query = query + """ AND """ + """publications.tsv @@ websearch_to_tsquery('""" + str(tak) + """')"""
+
+	query = query + """ ORDER BY year DESC"""
+
+	return query
